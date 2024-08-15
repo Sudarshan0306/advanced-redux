506aa9ea454a24050409e8734a9b1b48bc0edbad
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-08-15 21:09:11 +0530

    connected with firebase and sending data to firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 
 function App() {
-  const showCart = useSelector(state => state.ui.isCartVisible)
+  const cart = useSelector(state => state.cart)
+
+  const showCart = useSelector(state => state.ui.isCartVisible);
+
+  useEffect(()=> {
+    fetch("https://advanced-redux-7e984-default-rtdb.firebaseio.com/cart.json", {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
   return (
     <Layout>
       {showCart && <Cart />}
